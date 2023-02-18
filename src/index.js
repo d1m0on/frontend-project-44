c2ea1a49fad7270deb818dfd4e9be45970ccba7d
Дмитрий <musicx3m@yandex.ru>
2023-02-18 21:58:16 +0700

    src/index.js added and bin/brain-calc.js added

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import readlineSync from 'readline-sync';
+
+const rounds = 3;
+const startGame = (questionAndAnswer, description) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name?');
+  console.log(`Hello,${name}`);
+  console.log(description);
+  for (let i = 0; i < rounds; i += 1) {
+    const [question, answer] = questionAndAnswer();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${answer}.`);
+      console.log(`Let's try again, ${name}`);
+      return;
+    }
+  }
+  console.log(`Congratulations ${name}!`);
+};
+
+export default startGame;
