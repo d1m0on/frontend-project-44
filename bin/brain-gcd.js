3b12ad6b1977e71db11b3c48ec1872824ba76de0
Дмитрий <musicx3m@yandex.ru>
2023-03-12 20:36:46 +0700

    brain-gcd changed

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,7 +1,28 @@
 #!/usr/bin/env node
 
-import startGame from '../src';
+import readlineSync from 'readline-sync';
 
+// логика игры
+const rounds = 3;
+const startGame = (questionAndAnswer, description) => {
+  console.log('Welcome to the Brain Games!');
+  const name = readlineSync.question('May I have your name?');
+  console.log(`Hello,${name}`);
+  console.log(description);
+  for (let i = 0; i < rounds; i += 1) {
+    const [question, answer] = questionAndAnswer();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === answer) {
+      console.log('Correct!');
+    } else {
+      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${answer}.`);
+      console.log(`Let's try again, ${name}`);
+      return;
+    }
+  }
+  console.log(`Congratulations ${name}!`);
+};
 // функция получения случайного числа
 const randomNumber = (min = 1, max = 99) => Math.round(Math.random() * (max - min) + min);
 // функция получения случайного выражения
